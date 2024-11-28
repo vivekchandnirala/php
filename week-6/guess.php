2bd0497b959233dd7bcff0c46ea76aa073f31d8d
VIVEK CHAND NIRALA <121417084+vivekchandnirala@users.noreply.github.com>
2024-11-29 00:50:25 +0530

    Update guess.php

--- week-6/guess.php
+++ week-6/guess.php
@@ -5,29 +5,23 @@
   <h1>Welcome to my guessing game</h1>
 
   <?php
-  $correctnumber = 48; // Correct number to guess
+  $correctnumber = 48; 
   
-  // Check if the 'guess' parameter is set in the URL
   if (isset($_GET['guess'])) {
     $guess = $_GET['guess'];
 
-    // Check if the guess is a valid number
     if (!is_numeric($guess)) {
       echo "Your guess is not a number. Please enter a valid number.";
     }
-    // Check if the guess is too short (empty or less than 1 character)
     else if (strlen($guess) < 1) {
       echo "Your guess is too short. Please enter a valid guess.";
     }
-    // Check if the guess is too low
     else if ($guess < $correctnumber) {
       echo "Your guess is too low. Try again!";
     }
-    // Check if the guess is too high
     else if ($guess > $correctnumber) {
       echo "Your guess is too high. Try again!";
     }
-    // If the guess is correct
     else if ($guess == $correctnumber) {
       echo "Congratulations - You are right!";
     }
